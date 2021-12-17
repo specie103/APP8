d010a9348adf22449f58b52f80096674c789b113
specie103 <thelaw103@gmail.com>
2021-12-17 17:05:49 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function handleBulbClicks() {
+    $('.lightbulb').click(function(event){
+      $('.lightbulb').removeClass('bulb-on');
+   $(event.currentTarget).addClass('bulb-on')
+    });  
+  }
+  
+  
+  $(function() {
+    handleBulbClicks();
+  });
